7c1fdf4f9d1607cd4fd2a14cc6759183c9b0eb74
Jason Jones <jones0401@gmail.com>
2019-05-29 18:08:31 -0500

    Updated with new controllers and menu

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title></title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href='http://fonts.googleapis.com/css?family=Merriweather' rel='stylesheet' type='text/css'>
+        <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+
+        <!-- jquery from google source -->
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"> </script>
+        <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.1/jquery-ui.min.js"> </script>
+        
+        <!-- Link to Stylesheet -->
+        <link rel="stylesheet" href="./popup.css" type="text/css" />
+        <link rel="stylesheet" href="./home.css" type="text/css" />
+        <link rel="stylesheet" href="./menu.css" type="text/css" />
+
+        <!-- toastr -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css" type="text/css">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"> </script>
+
+        <!-- Link to Javascript code for page -->
+        <script type="text/javascript" language="Javascript" src="./popup.js"> </script>
+        <script type="text/javascript" language="Javascript" src="./functions.js"> </script>
+        
+    </head>
+    <body>
+        @yield('menu')
+
+        @yield('main', '')
+    </body>
+</html>
---
+++ resources/views/menu.blade.php
@@ -0,0 +1,41 @@
+ 
+	<ul class="drawer">
+		<!-- HOME -->
+	        <li><a href="./SMS.php">Home</a></li>
+
+	    <!--  MEMBERS  *separate for ease of access -->
+	    	<li>
+				<a href="./contacts.php">Manage</a>
+					<ul>
+						<li><a href="./contacts.php">Contacts</a></li>
+						<li><a href="./groups.php">Groups</a></li>
+						<li><a href="./hooks.php">Special Responses</a></li>
+					</ul>
+			</li>
+
+		<!-- TEXT RESPONSES -->
+		<li>
+				<a href="./special.php">Responses</a>
+					<ul>
+						<li><a href="./special.php">Special</a></li>
+						<li><a href="./general.php">General</a></li>
+						<li><a href="./errors.php">Errors</a></li>
+					</ul>
+			</li>
+			
+		<!-- PROGRAM SETTINGS -->
+ 			<li>
+ 				<a href="#">Settings</a>
+ 				<ul>
+					<li><a href="./Logs.php">Logs</a></li>
+					<li><a href="./pricing.php">Pricing</a></li>
+				</ul>
+			</li>
+
+	</ul>
+
+	<div class="logout">
+		 	<br />
+ 		<a href="login/Logout.php">Logout</a>
+	</div>
+	
